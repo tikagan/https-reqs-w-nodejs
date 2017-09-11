663b81fc5d97c760034ca9d5df865873a84ad18a
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-11 15:02:00 +0000

    step 1: printing in chunks

--- httpsreqs.js
+++ httpsreqs.js
@@ -1,17 +1,21 @@
 var https = require('https')
 
-var requestOptions = {
-  host:'',
-  path: '/'
+function getAndPrintHTMLChunks(){
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  }
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8')
+    response.on('data', function(data) {
+      console.log('Chunk Recieved. Length:', data.length)
+      console.log('Chunk:', data, '\n')
+    })
+    response.on('end', function() {
+      console.log('Response stream complete.')
+    })
+  })
 }
 
-https.get(requestOptions, function (response) {
-  response.setEncoding('utf8')
-  response.on('data', function(data) {
-    console.log('Chunk Recieved. Length:', data.length)
-  })
-  response.on('end', function() {
-    console.log('Response stream complete.')
-  })
-})
-
+getAndPrintHTMLChunks()
